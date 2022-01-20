4a47210772e0f7210d4341b1c447bbe3f3df6e5a
ReubenHawley <reubenhawley@protonmail.com>
2022-01-20 15:54:45 +0100

    Initial commit

---
+++ src/basicPart1/JavaBasics1.java
@@ -0,0 +1,214 @@
+package basicPart1;
+
+import java.text.DecimalFormat;
+
+public class JavaBasics1 {
+    public static void printExerciseNum(int num) {
+        String corner = "*";
+        String border = "-------------";
+        if (num < 9) {
+            System.out.println(corner + border + corner + "\n" + "- Exercise " + num + ": -\n" + corner + border + corner);
+        } else if ((num > 9) && (num < 99)) {
+            System.out.println(corner + border + "-" + corner + "\n" + "- Exercise " + num + ": -\n" + corner + border + "-" + corner);
+        } else {
+            System.out.println(corner + border + "--" + corner + "\n" + "- Exercise " + num + ": -\n" + corner + border + "--" + corner);
+        }
+
+
+    }
+
+    public static int convertDecimalToBinary(int number) {
+        String binary = "";
+        while (number >= 1) {
+            if (number % 2 == 0) {
+                number = number / 2;
+                binary += "0";
+            } else {
+                binary += "1";
+                number = number / 2;
+            }
+        }
+        String rev = new StringBuffer(binary).reverse().toString();
+        return Integer.parseInt(rev);
+    }
+
+    public static int convertBinaryToDecimal(int number) {
+        String binary = Integer.toString(number);
+        String rev = new StringBuffer(binary).reverse().toString();
+        int decimal = 0;
+        for (int i = rev.length()-1; i >= 0; i--) {
+            if (rev.charAt(i)=='1') {
+                decimal+=Math.pow(2,i);
+            }
+        }
+        return decimal;
+    }
+
+    private static String convertDecimalToHexa(int num) {
+        String[] letter = {"A","B","C","D","E","F"};
+        if (num<10)
+            return Integer.toString(num);
+        else{
+            num-=10;
+            return letter[num];
+        }
+    }
+    public static void splitString(){
+        String binary = "10000";
+        int count = binary.length();
+        String[] binaries = new String[(count/4)+1];
+        binaries[0] = binary.substring(count-4,count);
+        binary = binary.substring(0, binary.length()-4);
+        System.out.println("Extracted:"+binaries[0]+" remaining string to be parsed "+binary);
+    }
+
+    public static void exercise1(String name) {
+        System.out.println("Hello");
+        System.out.println(name);
+    }
+
+    public static void exercise2(int one, int two) {
+        System.out.println(one + two);
+    }
+
+    public static void exercise3(int one, int two) {
+        try {
+            System.out.println(one / two);
+        } catch (AbstractMethodError e) {
+            System.err.println("AbstractMethodError: " + e.getMessage());
+        }
+    }
+
+    public static void exercise4() {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+
+    public static void exercise5(int one, int two) {
+        try {
+            System.out.println(one * two);
+        } catch (AbstractMethodError e) {
+            System.err.println("AbstractMethodError: " + e.getMessage());
+        }
+    }
+
+    public static void exercise6(int one, int two) {
+        try {
+            System.out.println(one + " + " + two + " = " + (one + two));
+            System.out.println(one + " - " + two + " = " + (one - two));
+            System.out.println(one + " x " + two + " = " + (one * two));
+            System.out.println(one + " / " + two + " = " + (one / two));
+            System.out.println(one + " mod " + two + " = " + (one % two));
+        } catch (AbstractMethodError e) {
+            System.err.println("AbstractMethodError: " + e.getMessage());
+        }
+    }
+
+    public static void exercise7(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (i * num));
+        }
+    }
+
+    public static void exercise8() {
+        System.out.println("   J    a   v     v  a                                                  \n" +
+                "   J   a a   v   v  a a                                                 \n" +
+                "J  J  aaaaa   V V  aaaaa                                                \n" +
+                " JJ  a     a   V  a     a");
+    }
+
+    public static void exercise9() {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+
+    public static void exercise10() {
+        System.out.println(4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+    }
+
+    public static void exercise11(double radius) {
+        double area = Math.PI * Math.pow(radius, 2);
+        double perimeter = 2 * (Math.PI * radius);
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+
+    }
+
+    public static void exercise12(int a, int b, int c) {
+        int avg = (a + b + c) / 3;
+        System.out.println(avg);
+
+    }
+
+    public static void exercise13(double width, double height) {
+        DecimalFormat f = new DecimalFormat("##.00");
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        System.out.println("Area is " + width + " * " + height + " = " + f.format(area));
+        System.out.println("Perimeter is 2 *(" + width + " + " + height + ") = " + f.format(perimeter));
+    }
+
+    public static void exercise14() {
+        String even = "* * * * * * ";
+        String odd = " * * * * *  ";
+        String lineShort = "==================================";
+        String flag = "";
+        for (int i = 0; i < 15; i++) {
+            if (i < 9) {
+                if (i % 2 == 0) {
+                    flag = flag + even + lineShort + "\n";
+                } else {
+                    flag = flag + odd + lineShort + "\n";
+                }
+            } else {
+                for (int j = 0; j < even.length(); j++) {
+                    flag = flag + "=";
+                }
+                flag = flag + lineShort + "\n";
+            }
+        }
+        System.out.println(flag);
+
+    }
+
+    public static void exercise15(int a, int b) {
+        System.out.println("input: a=" + a + " b=" + b);
+        int c = b;
+        b = a;
+        a = c;
+        System.out.println("output: a=" + a + " b=" + b);
+    }
+
+    public static void exercise16() {
+        System.out.println(" +\"\"\"\"\"+                                                 \n" +
+                "[| o o |]                                                \n" +
+                " |  ^  |                                                 \n" +
+                " | '-' |                                                 \n" +
+                " +-----+");
+    }
+    public static void exercise17(int one, int two) {
+        int binarySum = convertBinaryToDecimal(one)+convertBinaryToDecimal(two);
+        int sum = convertDecimalToBinary(binarySum);
+        System.out.println("Binary number is: "+sum);
+    }
+    public static void exercise18(int one, int two){
+        int binarySum = convertBinaryToDecimal(one)*convertBinaryToDecimal(two);
+        int sum = convertDecimalToBinary(binarySum);
+        System.out.println("Product of two binary numbers: "+sum);
+    }
+    public static void exercise19(int num){
+        System.out.println("Binary  Number: "+convertDecimalToBinary(num));
+    }
+
+    public static void exercise20(int num){
+
+        String hexa = convertDecimalToHexa(num);
+        System.out.println("Hexadecimal number is : "+hexa);
+    }
+
+    public static void exercise22(int num){
+        System.out.println("Decimal Number: "+convertBinaryToDecimal(num));
+    }
+}
---
+++ src/basicPart1/Main.java
@@ -0,0 +1,72 @@
+package basicPart1;
+
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaBasics1.printExerciseNum(1);
+        JavaBasics1.exercise1("Alexandra Abramov");
+
+        JavaBasics1.printExerciseNum(2);
+        JavaBasics1.exercise2(74, 36);
+
+        JavaBasics1.printExerciseNum(3);
+        JavaBasics1.exercise3(50, 3);
+
+        JavaBasics1.printExerciseNum(4);
+        JavaBasics1.exercise4();
+
+        JavaBasics1.printExerciseNum(5);
+        JavaBasics1.exercise5(25, 5);
+
+        JavaBasics1.printExerciseNum(6);
+        JavaBasics1.exercise6(125, 24);
+
+        JavaBasics1.printExerciseNum(7);
+        JavaBasics1.exercise7(8);
+
+        JavaBasics1.printExerciseNum(8);
+        JavaBasics1.exercise8();
+
+        JavaBasics1.printExerciseNum(9);
+        JavaBasics1.exercise9();
+
+        JavaBasics1.printExerciseNum(10);
+        JavaBasics1.exercise10();
+
+        JavaBasics1.printExerciseNum(11);
+        JavaBasics1.exercise11(7.5);
+
+        JavaBasics1.printExerciseNum(12);
+        JavaBasics1.exercise12(5, 6, 7);
+
+        JavaBasics1.printExerciseNum(13);
+        JavaBasics1.exercise13(5.6, 8.5);
+
+        JavaBasics1.printExerciseNum(14);
+        JavaBasics1.exercise14();
+
+        JavaBasics1.printExerciseNum(15);
+        JavaBasics1.exercise15(10, 2);
+
+        JavaBasics1.printExerciseNum(16);
+        JavaBasics1.exercise16();
+
+        JavaBasics1.printExerciseNum(17);
+        JavaBasics1.exercise17(10, 11);
+
+        JavaBasics1.printExerciseNum(18);
+        JavaBasics1.exercise18(10, 11);
+
+        JavaBasics1.printExerciseNum(19);
+        JavaBasics1.exercise19(16);
+
+        JavaBasics1.printExerciseNum(20);
+        JavaBasics1.exercise20(8);
+
+        System.out.println("testing split string for hexa function");
+        JavaBasics1.splitString();
+
+
+    }
+}
